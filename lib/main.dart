d72684ce7d129dfba4f1f78119959e7d9ae7d3fd
akuuzume <akuuzume@gmail.com>
2025-06-05 00:47:30 +0000

    Connecting the firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:mobile_app/src/pages/splash.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then(
-    (fn) {
-      runApp(const MainApp());
-    },
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
   );
+  runApp(const MainApp());
 }
 
 class MainApp extends StatelessWidget {
